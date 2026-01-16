7941bbcfe6ce915b9c5b094143b8655f4b7152aa
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-01-16 11:44:18 +0530

    Add Forloop

---
+++ Forloop_2.java
@@ -0,0 +1,31 @@
+class Forloop_2 {
+	public static void main (String args[]){
+		for (int x=1 ; x<=5;x++){
+			int y=1;
+			while (y<=x){
+				System.out.print(y);
+				y++;
+			
+			
+				
+			
+			}
+			System.out.println("");
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+
+
+
+
+
+
+
+
+}
