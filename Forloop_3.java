bea5ff83e4eb363b536db13d4c6c03fd2c3595f8
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-01-16 11:48:27 +0530

    Forloop_3

---
+++ Forloop_3.java
@@ -0,0 +1,31 @@
+class Forloop_3 {
+	public static void main (String args[]){
+		for (int x=5 ; x>=1;x--){
+			int y=1;
+			while (y<=x){
+				System.out.print(y);
+				y++;
+			
+			
+				
+			
+			}
+			System.out.println("");
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+
+
+
+
+
+
+
+
+}
