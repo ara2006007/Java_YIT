94e3eb4c4ab02f6620fb3e4a7aaad41944fa07ba
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-01-19 08:54:53 +0530

    while loops

---
+++ Do_while1.java
@@ -0,0 +1,26 @@
+class Do_while1 {
+	public static void main (String args[]){
+		int x = 1 ;
+		do {
+			System.out.print(x);
+			x++;
+			
+		}
+		while (x<=10);
+			
+			
+		
+		
+		
+		
+	}
+	
+
+
+
+
+
+
+
+
+}
---
+++ Do_while2.java
@@ -0,0 +1,31 @@
+class Do_while2 {
+	public static void main (String args[]){
+		int x = 1 ;
+		String y ="";
+		
+		do {
+			int n = x%2 ;
+			y=((n==0)?"*":""+x);
+			System.out.print(y);
+			
+			x++;
+			
+		}
+		while (x<=5);
+			
+			
+		
+		
+		
+		
+	}
+	
+
+
+
+
+
+
+
+
+}
